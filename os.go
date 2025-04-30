fda8d00d2d40825a6a1da4772617016bc47d8dde
Daming Yang <daming.yang@centurygame.com>
2025-04-30 12:14:27 +0800

    refactor: getwd fail sometimes

--- os.go
+++ os.go
@@ -2,8 +2,23 @@ package migration
 
 import (
 	"fmt"
+	"github.com/sandwich-go/boost/xos"
 	"os"
 )
+
+var (
+	CwdDir = "/usr/local/bin"
+)
+
+func init() {
+	if xos.IsGoRun() {
+		CwdDir = "/tmp"
+	} else {
+		if cwd, err := os.Getwd(); err == nil {
+			CwdDir = cwd
+		}
+	}
+}
 
 func Chdir(dest string, logger *Logger) (deferFunc func(), err error) {
 	deferFunc = func() {}
@@ -13,8 +28,12 @@ func Chdir(dest string, logger *Logger) (deferFunc func(), err error) {
 			logger.Info(fmt.Sprintf("Chdir from %s back to %s", dest, cwdDir))
 			_ = os.Chdir(cwdDir)
 		}
-	} else {
-		return deferFunc, fmt.Errorf("got err: %s get current dir", err.Error())
+	} else if CwdDir != "" {
+		cwdDir = CwdDir
+		deferFunc = func() {
+			logger.Info(fmt.Sprintf("Chdir from %s back to %s", dest, CwdDir))
+			_ = os.Chdir(CwdDir)
+		}
 	}
 	logger.Info(fmt.Sprintf("Chdir from %s to %s", cwdDir, dest))
 	if err = os.Chdir(dest); err != nil {
